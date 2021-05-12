bb0fc8b4317f3c5b708d717a516e29a882c5095e
Moreas <florent.pignaud@pfj-services.com>
2021-05-12 11:38:05 -0400

    react front end set up without backend conection

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react'
+import ContactContext from '../../context/contact/contactContext'
+import ContactItem from './ContactItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext)
+
+  const { contacts, filtered } = contactContext
+
+  if (contacts.length === 0) {
+    return <h4>Please Add a Concact</h4>
+  }
+
+  return (
+    <>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
+    </>
+  )
+}
+
+export default Contacts
